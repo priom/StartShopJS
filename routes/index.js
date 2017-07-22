f851f9c55e2f5da499885b3763d05f0de3896028
priom <priom.galaxy@gmail.com>
2017-07-22 15:48:17 -0400

    refactor

--- routes/index.js
+++ routes/index.js
@@ -7,15 +7,21 @@ const csrfProtect = csrf();
 
 router.use(csrfProtect);
 
+const title = 'Linkin Store'
+
 /* GET home page. */
 router.get('/', (req, res, next) => {
   Product.find((err, product) => {
-      res.render('index', { title: 'StartShopJS', products: product });
+      res.render('index', { title: title, products: product });
   });
 });
 
 router.get('/user/register', (req, res, next) => {
-    res.render('user/register', { csrfToken: req.csrfToken() });
+    res.render('user/register', { title: title, csrfToken: req.csrfToken() });
+});
+
+router.post('/user/register', (req, res, next) => {
+    res.redirect('/');
 });
 
 module.exports = router;
